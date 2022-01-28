80164da2863432d0745cd4a3cb6c0c3944e0d98a
Joshua Liu <supajoshjunk@gmai.com>
2022-01-28 10:57:27 -0500

    next dev

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:any*',
-        destination: '/',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:any*',
+  //       destination: '/',
+  //     },
+  //   ];
+  // },
 }
